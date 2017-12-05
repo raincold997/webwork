edd7a9326604f8548a986c00630bf2964930fc78
151250122 <151250122@samil.nju.edu.cn>
2017-12-05 18:17:20 +0800

    registe test

--- app.js
+++ app.js
@@ -4,15 +4,54 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var url = require('url');
+var sqlite3 = require('sqlite3');
+var sqlHelper = require('./database/sqlHelper');
+var async = require('async')
+
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+sqlHelper.connect(function (err) {
+    if(err) throw err;
+});
+
+app.on('close',function(err){
+    sqlHelper.disconnect(function (err) {
+        
+    });
+});
+
+
+
+app.use(session({
+    secret: 'secret',
+    cookie:{
+        maxAge: 1000*60*30
+}
+}));
+
+app.use(function(req,res,next){
+    res.locals.user = req.session.user;   // 从session 获取 user对象
+    var err = req.session.error;   //获取错误信息
+    delete req.session.error;
+    res.locals.message = "";   // 展示的信息 message
+    if(err){
+        res.locals.message = '<div class="alert alert-danger" style="margin-bottom:20px;color:red;">'+err+'</div>';
+    }
+    next();  //中间件传递
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine("html",require("ejs").__express);
+//app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,6 +60,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req,res,next) {
+    var url = req.originalUrl;
+    if(url !== "/users/login" && !req.session.user){
+        return res.redirect("/user/login");
+    }
+    next();
+})
 
 app.use('/', index);
 app.use('/users', users);
---
+++ database/userDataHelper.js
@@ -0,0 +1,141 @@
+//user数据库接口库
+var util = require('util');
+var sqlHelper = require('sqlHelper');
+var async = require('async');
+var tableName = "users";
+var colNames = ["userName", "password"];
+var keyName = "userName";
+
+/*
+添加一个用户
+ */
+exports.addAUser = function (values, callback) {
+    let user = null;
+
+    // async.series({
+    //     findUser: function (done) {
+    //         sqlHelper.findNoteById(tableName,keyName,values[0],
+    //             function (err,userName) {
+    //                 if(err) throw err;
+    //                 user = userName;
+    //             },function (err) {
+    //                 if(err) throw done(err,null);
+    //                 done(null);
+    //             });
+    //     },
+    //     addUser: function (done) {
+    //         if(user === null) {
+    //            sqlHelper.add(tableName,colNames,values,
+    //                function (err) {
+    //                    if(err){
+    //                        console.log(values[0]+' add fail with '+err);
+    //                        callback(err,0);
+    //                    }
+    //                    else{
+    //                        callback(null,1);
+    //                    }
+    //                    done(err)
+    //                })
+    //         }else{
+    //             callback(null,2);
+    //             done()
+    //         }
+    //     }
+    // },function (err,result) {
+    //     if(err) throw err;
+    // })
+
+    async.series([function (cb) {
+        //顺序执行的代码一
+        sqlHelper.findNoteById(tableName, keyName, values[0],
+            function (error, result) {
+                if (error) throw error;
+                // console.log("users1:" + result.userName);
+                user = result;
+                // cb(null,result);
+            },
+            function (error) {
+                if (error) throw cb(error, null);
+                //回调函数中也要传入cb，否则如果cb放在回调函数之外则无法完成同步
+                cb(null);
+            });
+    }, function (cb) {
+        //顺序执行的代码二
+        //0代表error,1代表成功添加用户,2代表用户已存在无法添加
+        if (user === null) {
+            sqlHelper.add("users", colNames, values,
+                function (error) {
+                    if (error) {
+                        util.log('Fail on add a user:' + values[0] + 'because of error:' + error);
+                        callback(error, 0);
+                    }
+                    else {
+                        callback(null, 1);
+                        // console.log("exist");
+                    }
+                    //回调函数中也要传入cb，否则如果cb放在回调函数之外则无法完成同步
+                    cb(error);
+                });
+        }
+        else {
+            callback(null, 2);
+            cb();
+        }
+
+    }], function (error, values) {
+        // console.log("zzz")
+        if (error) throw error;
+    });
+
+}
+/*
+用户登录判断
+ */
+exports.loginAUser = function (values, callback) {
+    let user = null;
+    // let userName = null;
+    // console.log("userName2:"+values[0]);
+    async.series([function (cb) {
+        sqlHelper.findNoteById(tableName, keyName, values[0],
+            function (error, result) {
+                if (error) throw cb(error, null);
+                // console.log("users1:" + result.userName);
+                user = result;
+                // cb(null,result);
+            },
+            function (error) {
+                if (error) throw cb(error, null);
+                cb(null);
+            });
+    }, function (cb) {
+        //0代表error,1代表成功登录,2代表用户不存在无法登录,3代表用户密码错误
+        if (user === null) {
+            sqlHelper.add("users", colNames, values,
+                function (error) {
+                    if (error) {
+                        util.log('Fail on add a user:' + values[0] + 'because of error:' + error);
+                        callback(error, 0, user);
+                    }
+                    else {
+                        callback(null, 2, user);
+                        // console.log("exist");
+                    }
+                    cb(error);
+                });
+        }
+        else {
+            // console.log("user exist" + user.password + "values[1]" + values[1]);
+            if (user.password === values[1]) {
+                callback(null, 1, user);
+            }
+            else {
+                callback(null, 3, user);
+            }
+            cb();
+        }
+    }], function (error, values) {
+        // console.log("zzz")
+        if (error) throw error;
+    });
+
+}
